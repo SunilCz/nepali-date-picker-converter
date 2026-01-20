81334a1172ccf2a9d7d9309cd6f5c77c8252e142
Sunil Chaudhary <sunil.chaudhary@amniltech.com>
2026-01-20 11:46:22 +0545

    Standardize exports and fix import paths (v0.1.7)

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,42 @@
-import * as converter from "./core/converter";
-import * as types from "./core/types";
-import * as formatter from "./utils/formatter";
+// 1. Import everything first
+import { adToBs, bsToAd, formatBs, formatAd } from "./core/converter";
 import { NepaliDate } from "./core/NepaliDate";
-import { NepaliDatePicker } from "./components/DatePicker";
+import { NepaliDatePicker, NepaliDatePickerProps } from "./components/DatePicker";
 import { mountNepaliDatePicker } from "./utils/mount";
+import { toNepaliNumeral, toEnglishNumeral } from "./utils/formatter";
 
-// Named exports
-export * from "./core/converter";
+// 2. Named Exports (Always recommended)
+export {
+  adToBs,
+  bsToAd,
+  NepaliDate,
+  NepaliDatePicker,
+  NepaliDatePickerProps,
+  mountNepaliDatePicker,
+  formatBs,
+  formatAd,
+  toNepaliNumeral,
+  toEnglishNumeral
+};
+
 export * from "./core/types";
-export * from "./utils/formatter";
-export { NepaliDate } from "./core/NepaliDate";
-export { NepaliDatePicker, NepaliDatePickerProps } from "./components/DatePicker";
-export { mountNepaliDatePicker } from "./utils/mount";
 
-export const VERSION = "0.1.6";
+export const VERSION = "0.1.7";
 export const NAME = "nepali-date-picker-converter";
 
-// Default export for developer convenience
-export default {
-  ...converter,
-  ...types,
-  ...formatter,
+// 3. Default Export (For convenience)
+const NepaliDatePickerConverter = {
+  adToBs,
+  bsToAd,
   NepaliDate,
   NepaliDatePicker,
   mountNepaliDatePicker,
+  formatBs,
+  formatAd,
+  toNepaliNumeral,
+  toEnglishNumeral,
   VERSION,
   NAME
 };
+
+export default NepaliDatePickerConverter;
